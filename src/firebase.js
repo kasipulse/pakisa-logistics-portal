df064e068be3394316ffe9793fd8fbcbc30a8469
kasipulse <148783028+kasipulse@users.noreply.github.com>
2026-07-13 13:07:20 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; // <-- Add this import
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBc9Hs-Oef3hSuuRY1KWiP51E8oVy4yKik",
+  authDomain: "tripsheet-9ea74.firebaseapp.com",
+  projectId: "tripsheet-9ea74",
+  storageBucket: "tripsheet-9ea74.firebasestorage.app",
+  messagingSenderId: "610091199059",
+  appId: "1:610091199059:web:99bc24ed74e78d899834a6",
+  measurementId: "G-TVYK9KF102"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app); // <-- Add this export
